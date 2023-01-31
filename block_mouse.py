7d3237ff1c501abfec6a237200654df5bf705cb1
Andy Fraley <andrew.fraley@gmail.com>
2023-01-30 22:19:14 -0500

    unknown

--- block_mouse.py
+++ block_mouse.py
@@ -4,7 +4,7 @@ import os
 import time
 
 INPUT_DEVICE = "/dev/input/event3"
-
+OUTPUT_DEVICE = "/dev/uinput"
 
 EVIOCGRAB = 1074021776
 
@@ -13,14 +13,15 @@ def main():
     device = INPUT_DEVICE
     input_format = "llHHI"
     event_size = struct.calcsize(input_format)
-    device_handle = open(device, "rb")
-    fcntl.ioctl(device_handle, EVIOCGRAB, True)
+    inputfd = open(device, "rb")
+    outputfd = open(OUTPUT_DEVICE, "wb")
+    fcntl.ioctl(inputfd, EVIOCGRAB, True)
     ignored_codes = ["1198", "1199"]
     forwarded_events = {}
 
     while True:
         now = time.time()
-        event = device_handle.read(event_size)
+        event = inputfd.read(event_size)
 
         if event in forwarded_events:
             del(forwarded_events[event])
@@ -30,18 +31,9 @@ def main():
         print("code: %s  value: %s" % (code, value))
         if code not in ignored_codes:
             forwarded_events[event] = now
-            fcntl.ioctl(device_handle, EVIOCGRAB, False)
-            send_input_event(device, event)
-            fcntl.ioctl(device_handle, EVIOCGRAB, True)
+            outputfd.write(event)
         else:
             print('ignoring code %s' % code)
-
-
-def send_input_event(device, event):
-    """ Send the event back to the input device """
-    out_file = os.open(device, os.O_RDWR)
-    os.write(out_file, event)
-    os.close(out_file)
 
 
 if __name__ == "__main__":
